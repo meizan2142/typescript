69c360786b47e411a29c7801c4b22f63f31d2fd1
Your Name01~git <saifsahai2002@gmail.com>
2025-11-17 20:48:25 +0600

    some problems are solved

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-* Blog Post
+# TypeScript এর মধ্যে Interface এবং Type-এর পার্থক্য: 
+* Interface শুধুমাত্র object-এর কাঠামো বা shape নির্ধারণ করতে ব্যবহৃত হয় । আর Type ব্যবহার করা হয় Primitive, Union, Tuple, * * Function-type ইত্যাদি এগুলোর জন্য।
+* একাধিকবার একই নামের interface declare করলে সবগুলো merge হয়ে যায়। আর Type-এর ক্ষেত্রে এটি সম্ভব নয়, একই নামের type declare করলে error হয়।
+* Interface-এ intersection ব্যবহার করা যায় না। আর Type-এর মধ্যে intersection ব্যবহার করে অনেকগুলো value-type একসাথে দেওয়া যায়। যেমন: type Book = string | number | boolean
+
+
+# Enums in Typescript: 
+* TypeScript-এ enum ব্যবহার করা হয় নির্দিষ্ট কিছু মানকে একজোট করতে। যখন কোডে কোনও মান বারবার ব্যবহার করতে হবে এবং সেই মানগুলোর জন্য readable ও meaningful নাম দরকার হয়—তখন enum খুবই কাজে লাগে।
+
+### String enum
+```ts
+enum Status {
+  Success = "SUCCESS",
+  Error = "ERROR",
+  Pending = "PENDING"
+}
+const currentStatus = Status.Success;
+console.log(currentStatus);
+```
+
+### Numeric enum
+```ts
+enum CarName {
+    Honda,
+    Toyota,
+    Alto,
+    Swift,
+}
+console.log(CarName);
+console.log("Value of Alto is : "+ CarName.Alto);
+```
+
--- solution.ts
+++ solution.ts
@@ -1,4 +1,105 @@
-// * Problem - 1
+type InputType = string | number | boolean;
+function formatValue(input: InputType): InputType {
+    if (typeof input === "string") {
+        return input.toUpperCase()
+    }
+    else if (typeof input === "number") {
+        return input * 10;
+    }
+    else if (typeof input === "boolean") {
+        if (input === true) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+    return input;
+}
 
-console.log("First assignment of Typescript begins")
+
+
+type LengthType = string | any[];
+function getLength(input: LengthType): number {
+    if (typeof input === "string") {
+        return input.length;
+    } else if (Array.isArray(input)) {
+        return input.length;
+    } else {
+        throw new Error("Invalid input type");
+    }
+}
+
+
+
+class Person {
+    name: string;
+    age: number;
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+    }
+    getDetails() {
+        return `'Name: ${this.name}, Age: ${this.age}'`
+    }
+}
+
+
+
+type BookType = {
+    title: string;
+    rating: number;
+}
+function filterByRating(books: BookType[]): BookType[] {
+    return books.filter((book) => book.rating >= 4)
+}
+
+
+
+type UserType = {
+    id: number;
+    name: string;
+    email: string;
+    isActive: boolean;
+}
+function filterActiveUsers(users: UserType[]): UserType[] {
+    return users.filter((user) => user.isActive === true)
+}
+
+
+
+interface Book {
+    title: string;
+    author: string;
+    publishedYear: number;
+    isAvailable: boolean;
+}
+function printBookDetails(book: Book): string {
+    const availability = book.isAvailable ? "Yes" : "No";
+    const result = `Title: ${book.title}, Author: ${book.author}, Published: ${book.publishedYear}, Available: ${availability}`;
+    return result;
+}
+
+type Product = {
+    name: string;
+    price: number;
+    quantity: number;
+    discount?: number;
+};
+
+function calculateTotalPrice(products: Product[]): number {
+    return products
+        .map((product) => {
+            const baseTotal = product.price * product.quantity;
+            if (product.discount !== undefined) {
+                const discountAmount = (baseTotal * product.discount) / 100;
+                return baseTotal - discountAmount;
+            }
+
+            return baseTotal;
+        })
+        .reduce((sum, current) => sum + current, 0);
+}
+
+
 
